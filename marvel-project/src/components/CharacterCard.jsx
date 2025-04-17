675a3a8c6df1c7d154bb0ed604b7c37361ae91aa
John <jgeiger@Mac.lan>
2025-04-17 12:36:33 -0400

    charactercard done for now

--- marvel-project/src/components/CharacterCard.jsx
+++ marvel-project/src/components/CharacterCard.jsx
@@ -1,4 +1,5 @@
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react'
 
 function titleCase(str) {
     return str
@@ -9,17 +10,46 @@ function titleCase(str) {
 }
 
 function CharacterCard({ name, alias, alignment, powers, image_url }) {
+    const [showMore, setShowMore] = useState(false);
+
+    const togglePowers = () => setShowMore(!showMore);
+
+    //if powers is greater than 100 characters, only show characters from index 0 to 100 of the string and then a ... at end
+   //if powers is less than 100, show all
+    const preview = powers.length > 25 ? powers.slice(0, 25) + '...' : powers; 
+
     return (
-        <Card style={{ width: '28rem' }} className="character-card mb-4 shadow-sm">
-            <Card.Img variant="top" src={image_url} alt={`${name} character image`} />
+        <Card style={{ width: '20rem' }} className="character-card mb-4 shadow-sm hover-zoom">
+            <Card.Img src={image_url} alt={`${name} character image`} />
             <Card.Body className="bg-primary text-white d-flex justify-content-center">  
                 <div className="text-start">
                     <Card.Title className="fw-bold fs-1 text-center mb-4 text-decoration-underline"> {name} </Card.Title>
                     <Card.Subtitle className="mb-2 text-light fs-4">
                         <strong>AKA: </strong>{titleCase(alias)}
                     </Card.Subtitle>
-                    <Card.Text className="mb-2 fs-4"><strong>Alignment: </strong>{alignment.charAt(0).toUpperCase() + alignment.slice(1)}</Card.Text>
-                    <Card.Text className="fs-4"><strong>Powers: </strong>{powers}</Card.Text>  
+                    <Card.Text className="mb-2 fs-4"><strong>Alignment: </strong>{titleCase(alignment)}</Card.Text>
+                    <Card.Text className="fs-4"
+                        style={{
+                            overflowX: 'hidden', //this makes it so there is no X overflow and no horizontal scroll bar, it stops card from expanding horizontally
+                            whiteSpace: 'pre-wrap', //this preserves the line breaks and wraps the text properly
+                            wordBreak: 'break-word', //this breaks long words into multible lines if necessary
+                            lineHeight: '1.5',
+                            maxHeight: showMore ? '150px' : 'auto',  // if showMore is true, it shows as normal
+                            overflowY: showMore ? 'auto': 'visible' //if showMore is false, it allows scroll
+
+                        }}
+                    >
+                        <strong>Powers: </strong>
+                        {showMore ? powers : preview}{' '}
+                        {powers.length > 25 && (
+                            <span
+                                onClick={togglePowers}
+                                style={{cursor: 'pointer', color: '#ffc107', fontWeight: 'bold'}}
+                            >
+                                {showMore ? 'Show Less' : 'Show More'}
+                            </span>
+                        )}
+                    </Card.Text>  
                 </div>
                 
             </Card.Body>
--- marvel-project/src/pages/Home.jsx
+++ marvel-project/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       name="John"
       alias="johnny be sexy"
       alignment="not sure"
-      powers="Will kill you with sexiness"
+      powers="Will kill you with sexiness aasdfasdfasdfasdfjhasdkfjhlaksdjf;lkashdflkjahsdlk asdfjh;la adf; a;dslkf as;dfkha sflkahsdf; ad;sfl asdl;fja;lsdfj ;aldjf ;alds  as;dflkha ;sdf;lasdf ;asldkf l; asdlkfja;sldf j;;laksjd; flkasdfj ;lasdj f;ldsakj f;lasdk fkl;lk a;sldfkja;lsdkfja;lsdkf a;lsdfj;alsd fj;lds jfldksa"
       image_url="https://placehold.co/300"
     />
   );
