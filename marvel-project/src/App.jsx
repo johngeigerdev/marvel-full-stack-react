3e40ea8918c10f9323a6ebcb10df6e299d8ea7e5
John <jgeiger@Mac.lan>
2025-04-20 21:41:12 -0400

    almost done with editcharacter.jsx

--- marvel-project/src/App.jsx
+++ marvel-project/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './index.css';
 import AddCharacter from './pages/AddCharacter';
+import EditCharacter from './pages/EditCharacter';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/add-character' element={<AddCharacter />} />
+        <Route path='/edit-character/:id' element={<EditCharacter />} />
       </Routes>
     </Router>
   )
