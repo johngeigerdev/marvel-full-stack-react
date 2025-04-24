48d6508c88e6fc155b215ff4d5679660d6119a23
John <jgeiger@Mac.lan>
2025-04-23 23:57:30 -0400

    about page styling done

--- marvel-project/src/pages/About.jsx
+++ marvel-project/src/pages/About.jsx
@@ -4,44 +4,48 @@ function About() {
 
     return(
         <Container fluid className="bg-dark text-center">
-            <Row className = "text-center">
-                <h3 className="about-heading"> Welcome to my module-7 full-stack Marvel project</h3>
+            <Row className = "justify-content-center text-center">
+                <h3 className="about-heading my-5"> Welcome to my module-7 full-stack Marvel project</h3>
             </Row>
-            <Row className="mb-4 justify-content-center">
-                <Col md="auto">
-                    <div style={{ padding: '0 1rem', maxWidth: '350px' }}>
-                        <Carousel>
+            <Row className="mb-5 justify-content-center">
+                <Col xs={12} className="d-flex justify-content-center">
+                    <div style={{ width: '100%', maxWidth: '350px' }}>  
+                        <Carousel className="mb-4">
                             <Carousel.Item>
-                                <img
-                                    className='d-block w-100'
-                                    src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Iron_Man_%28circa_2018%29.png/220px-Iron_Man_%28circa_2018%29.png"
-                                    alt="Iron Man"
-                                    style={{ height: '400px', objectFit: 'contain'}}
-                                />
+                                <div className="d-flex justify-content-center align-items-center" style={{ height: '400px' }}>
+                                    <img
+                                        src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Iron_Man_%28circa_2018%29.png/220px-Iron_Man_%28circa_2018%29.png"
+                                        alt="Iron Man"
+                                        style={{ height: '400px', objectFit: 'contain'}}
+                                    />
+                                </div>
+                                
                                 <Carousel.Caption style={{ textShadow: '2px 2px black' }} >
                                     <h3>Iron Man</h3>
                                 </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <img
-                                    className='d-block w-100'
-                                    src="https://upload.wikimedia.org/wikipedia/en/1/17/Thor_by_Olivier_Coipel.png"
-                                    alt="Iron Man"
-                                    style={{ height: '400px', objectFit: 'contain'}}
-                                />
+                                <div className="d-flex justify-content-center align-items-center" style={{ height: '400px' }}>
+                                    <img
+                                        src="https://upload.wikimedia.org/wikipedia/en/1/17/Thor_by_Olivier_Coipel.png"
+                                        alt="Thor"
+                                        style={{ height: '400px', objectFit: 'contain'}}
+                                    />
+                                </div>
                                 <Carousel.Caption style={{ textShadow: '2px 2px black' }} >
                                     <h3>Thor</h3>
                                 </Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <img
-                                    className='d-block w-100'
-                                    src="https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png"
-                                    alt="Spider Man"
-                                    style={{ height: '400px', objectFit: 'contain'}}
-                                />
+                                <div className="d-flex justify-content-center align-items-center" style={{ height: '400px' }}>
+                                    <img
+                                        src="https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png"
+                                        alt="Spider Man"
+                                        style={{ height: '400px', objectFit: 'contain'}}
+                                    />
+                                </div>
                                 <Carousel.Caption style={{ textShadow: '2px 2px black' }} >
-                                    <h3>Iron Man</h3>
+                                    <h3>Spider Man</h3>
                                 </Carousel.Caption>
                             </Carousel.Item>
                         </Carousel>
@@ -49,8 +53,8 @@ function About() {
                     
                 </Col>
             </Row>
-            <Row>
-                <Col className="about-text">
+            <Row className="justify-content-center text-center">
+                <Col xs={12} md={10} lg={8} className="about-text text-start py-5 px-4 mx-auto">
                     <p>
                         In this project, I used several Javascript and React concepts to put everything together. The back-end was 
                         provided so that students could focus on their front-end skills in this project. I took the back-end files and
